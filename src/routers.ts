d7cc00f4663774dba3e7e31ace2651336acb9176
Vlad <kirito1498@gmail.com>
2022-10-27 15:28:30 +0300

    fixed routers

--- src/routers.ts
+++ src/routers.ts
@@ -7,23 +7,26 @@ import { postsRouter } from "./modules/posts/routes/posts-router";
 import { deleteAllRouter } from "./common/routes/delete-router";
 import { usersRouter } from "./modules/users/routes/users-router";
 import { authRouter } from "./modules/users/routes/auth-router";
+import { commentsRouter } from "modules/comments/routes/comments-router";
 
 const router = {
   products: "/products",
   addresses: "/addresses",
-  ht_05: "/hometask_06/api",
+  ht_06: "/hometask_06/api",
   blogs: "/blogs",
   posts: "/posts",
+  comments: "/comments",
   users: "/users",
   auth: "/auth",
   delete_all: "/testing/all-data",
 } as const;
 
-const blogs_router = `${router.ht_05}${router.blogs}` as const;
-const posts_router = `${router.ht_05}${router.posts}` as const;
-const users_router = `${router.ht_05}${router.users}` as const;
-const auth_router = `${router.ht_05}${router.auth}` as const;
-const delete_all_router = `${router.ht_05}${router.delete_all}` as const;
+const blogs_router = `${router.ht_06}${router.blogs}` as const;
+const posts_router = `${router.ht_06}${router.posts}` as const;
+const users_router = `${router.ht_06}${router.users}` as const;
+const comments_router = `${router.ht_06}${router.comments}` as const;
+const auth_router = `${router.ht_06}${router.auth}` as const;
+const delete_all_router = `${router.ht_06}${router.delete_all}` as const;
 
 export const useRouters = (app: Express) => {
   app.use(router.products, productsRouter);
@@ -31,6 +34,7 @@ export const useRouters = (app: Express) => {
 
   app.use(blogs_router, blogsRouter);
   app.use(posts_router, postsRouter);
+  app.use(comments_router, commentsRouter);
   app.use(users_router, usersRouter);
   app.use(auth_router, authRouter);
 
