8699378f58c883add54c4eff21a9c12b8f9e0267
Vlad <kirito1498@gmail.com>
2022-11-09 16:54:50 +0300

    refactoring schemas

--- src/common/db/schemas.ts
+++ src/common/db/schemas.ts
@@ -7,53 +7,53 @@ import { UserDB } from "modules/users/user";
 import { RefreshSession } from "modules/auth/auth";
 
 export const blogSchema = new mongoose.Schema<Blog>({
-  id: String,
-  name: String,
-  youtubeUrl: String,
-  createdAt: Date,
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  youtubeUrl: { type: String, required: true },
+  createdAt: { type: String, required: true },
 });
 
 export const postsSchema = new mongoose.Schema<Post>({
-  id: String,
-  title: String,
-  shortDescription: String,
-  content: String,
-  blogId: String,
-  blogName: String,
-  createdAt: Date,
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  shortDescription: { type: String, required: true },
+  content: { type: String, required: true },
+  blogId: { type: String, required: true },
+  blogName: { type: String, required: true },
+  createdAt: { type: String, required: true },
 });
 
 export const commentsSchema = new mongoose.Schema<CommentDB>({
-  id: String,
-  content: String,
-  userId: String,
-  userLogin: String,
-  createdAt: Date,
-  postId: String,
+  id: { type: String, required: true },
+  content: { type: String, required: true },
+  userId: { type: String, required: true },
+  userLogin: { type: String, required: true },
+  createdAt: { type: String, required: true },
+  postId: { type: String, required: true },
 });
 
 export const usersSchema = new mongoose.Schema<UserDB>({
   accountData: {
-    id: String,
-    email: String,
-    login: String,
-    createdAt: Date,
-    passHash: String,
-    passSalt: String,
+    id: { type: String, required: true },
+    email: { type: String, required: true },
+    login: { type: String, required: true },
+    createdAt: { type: String, required: true },
+    passHash: { type: String, required: true },
+    passSalt: { type: String, required: true },
   },
   emailConfirmation: {
-    confirmationCode: String,
-    expirationDate: Date,
-    isConfirmed: Boolean,
+    confirmationCode: { type: String, required: true },
+    expirationDate: { type: Date, required: true },
+    isConfirmed: { type: Boolean, required: true },
   },
 });
 
 export const sessionsSchema = new mongoose.Schema<RefreshSession>({
-  refreshToken: String,
-  issuedAt: Number,
-  expiresIn: Number,
-  userId: String,
-  ip: String,
-  deviceId: String,
-  deviceName: String,
+  refreshToken: { type: String, required: true },
+  issuedAt: { type: Number, required: true },
+  expiresIn: { type: Number, required: true },
+  userId: { type: String, required: true },
+  ip: { type: String, required: true },
+  deviceId: { type: String, required: true },
+  deviceName: { type: String, required: true },
 });
