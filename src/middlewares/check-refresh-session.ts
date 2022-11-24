4f750736ec07f78845b2deb454def7e1476a531c
Vlad <kirito1498@gmail.com>
2022-11-24 12:28:44 +0300

    fixed for tests

--- src/middlewares/check-refresh-session.ts
+++ src/middlewares/check-refresh-session.ts
@@ -4,6 +4,7 @@ import { HTTP_STATUSES } from 'common/http-statuses';
 import { sessionsService } from 'modules/auth/services/sessions-service';
 import { usersQueryRepository } from 'modules/users/repositories';
 import { User } from 'modules/users/user';
+import { jwtService } from 'common/services/jwt-service';
 
 export const checkRefreshSession = async (req: Request, res: Response, next: NextFunction) => {
   const refreshToken = req.cookies?.refreshToken;
@@ -25,6 +26,14 @@ export const checkRefreshSession = async (req: Request, res: Response, next: Nex
 };
 
 export const setUserToContextBySession = async (req: Request, res: Response, next: NextFunction) => {
+  /** TODO: temporarily, only for external tests */
+  const token = req.headers.authorization?.split(' ')[1];
+  const userId = token ? await jwtService.getUserIdByToken(token) : null;
+
+  if (userId && req.requestContext.user?.id !== userId)
+    req.requestContext.user = (await usersQueryRepository.findUserById(userId)) as User;
+  /**************/
+
   const refreshToken = req.cookies?.refreshToken;
   const currentSession = await sessionsService.getByRefreshToken(refreshToken);
 
